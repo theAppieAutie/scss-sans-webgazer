c6ff3f7755b1501ad7fb809a6e2965615456289f
MorOtter <158565667+MorOtter@users.noreply.github.com>
2025-02-06 15:07:07 +0000

    Update dbServices.js

--- services/dbServices.js
+++ services/dbServices.js
@@ -5,13 +5,13 @@ const {Pool} = require('pg');
 // Set up connection to DB and CRUD ops
 
 // local development uncomment below
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
+//const pool = new Pool({
+//    user: process.env.DB_USER,
+//    host: process.env.DB_HOST,
+//    database: process.env.DB_DATABASE,
+//    password: process.env.DB_PASSWORD,
+//    port: process.env.DB_PORT,
+//  });
 
 
 //   hosted on heroku for my hosted version only
@@ -30,12 +30,12 @@ async () => {
 
 }
 
-//  const pool = new Pool({
-//     connectionString: process.env.SUPABASE_DB_URL,
-//      ssl:{
-//          rejectUnauthorized: false
-//      }
-//  });
+  const pool = new Pool({
+     connectionString: process.env.SUPABASE_DB_URL,
+      ssl:{
+          rejectUnauthorized: false
+      }
+  });
   
 pool.connect()
     .then(() => console.log('Connected to PostgreSQL'))
